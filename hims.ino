af20af6ad1584d9db5aaf914feb0fa607eea2742
ZeeHatcher <zee.hatcher010@gmail.com>
2021-04-12 13:29:09 +0800

    upload arduino sketches

---
+++ hims.ino
@@ -0,0 +1,75 @@
+#define PIN_RST 9
+#define PIN_SDA 10
+#define PIN_POT A0
+
+#define USE_TIMER_1     true
+#define TIMER1_INTERVAL_MS  5000
+
+#include "SPI.h"
+#include "MFRC522.h"
+#include "TimerInterrupt.h"
+
+MFRC522 rfid(PIN_SDA, PIN_RST); // Set up mfrc522 on the Arduino
+
+byte nuidPICC[4];
+
+void setup() {
+  SPI.begin(); // open SPI connection
+  rfid.PCD_Init(); // Initialize Proximity Coupling Device (PCD)
+  Serial.begin(9600); // open serial connection
+
+  ITimer1.init();
+  
+  if (!ITimer1.attachInterruptInterval(TIMER1_INTERVAL_MS, Timer1Handler))
+  {
+    Serial.println(F("Can't set ITimer1. Select another freq. or timer"));
+  }
+}
+
+void loop() {
+  // Check presence of new card
+  if (!rfid.PICC_IsNewCardPresent()) {
+    for (byte i = 0; i < sizeof(nuidPICC); i++) {
+      nuidPICC[i] = 0;
+    }
+    return;
+  }
+  
+  if (!rfid.PICC_ReadCardSerial()) return;
+
+  // Assign new value to NUID if different
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < sizeof(nuidPICC); i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+  }
+
+  // Halt PICC
+  //rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  //rfid.PCD_StopCrypto1();
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+void Timer1Handler() {
+  // Print recorded NUID if not "empty" / is not filled with 0s
+  if (nuidPICC[0] || nuidPICC[1] || nuidPICC[2] || nuidPICC[3]) {
+    printHex(nuidPICC, sizeof(nuidPICC));
+    Serial.print(":");
+    Serial.println(analogRead(PIN_POT));
+  }
+}
