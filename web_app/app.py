a4648dfdf3d29ad804365d878f4e98d778a6e00c
BuildTools <unconfigured@null.spigotmc.org>
2021-04-13 22:10:47 +0800

    add analytics

--- web_app/app.py
+++ web_app/app.py
@@ -11,6 +11,7 @@ def unix_time_millis(dt):
 
 app = Flask(__name__)
 db = MySQLdb.connect("localhost", "pi", "", "hims_db") or die("Could not connect to database")
+db.autocommit(True)
 
 @app.route("/")
 def index():
@@ -21,16 +22,14 @@ def weights():
     rows_items = []
     rows_weights = []
 
-    with db:
-        cursor = db.cursor()
+    with db.cursor() as cur:
+        cur.execute("SELECT * FROM items")
+        rows_items = cur.fetchall()
 
-        cursor.execute("SELECT * FROM items")
-        rows_items = cursor.fetchall()
+        cur.execute("SELECT * FROM weights")
+        rows_weights = cur.fetchall()
 
-        cursor.execute("SELECT * FROM weights")
-        rows_weights = cursor.fetchall()
-
-        cursor.close()
+        cur.close()
 
     res = {}
 
@@ -41,11 +40,15 @@ def weights():
             "name": name,
             "state": state,
             "threshold": threshold,
-            "weights": []
+            "weights": [],
+            "rate": None
         }
 
+    agg = {}
+    mem = {}
+
     for row in rows_weights:
-        nuid, weight, created_at = row
+        nuid, weight, created_at, is_begin_point, delta = row
 
         if nuid not in res:
             continue
@@ -54,8 +57,38 @@ def weights():
             "timestamp": created_at.isoformat(sep=" "),
             "weight": weight
         })
+        
+        if nuid not in agg:
+            agg[nuid] = {
+                "sum": 0,
+                "count": 0
+            }
 
-    #print(res)
+        if nuid not in mem:
+            mem[nuid] = {
+                "begin_weight": None,
+                "end_weight": None,
+                "duration": None
+            }
+
+        # Rate of change in weight
+        if is_begin_point:
+            if mem[nuid]["begin_weight"] != None and mem[nuid]["end_weight"] != None and mem[nuid]["duration"] != None:
+                agg[nuid]["sum"] += (mem[nuid]["begin_weight"] - mem[nuid]["end_weight"]) / mem[nuid]["duration"]
+                agg[nuid]["count"] += 1
+
+            mem[nuid] = {
+                "begin_weight": weight,
+                "end_weight": None,
+                "duration": None
+            }
+        else:
+            mem[nuid]["end_weight"] = weight
+            mem[nuid]["duration"] = delta
+
+    for nuid in agg:
+        if agg[nuid]["count"] > 0:
+            res[nuid]["rate"] = agg[nuid]["sum"] / agg[nuid]["count"]
 
     return jsonify(res)
 
@@ -64,12 +97,10 @@ def update(nuid):
     field = request.form["field"]
     value = request.form["value"]
 
-    with db:
-        cursor = db.cursor()
+    with db.cursor() as cur:
+        cur.execute("UPDATE items SET %s = '%s' WHERE id = '%s'" % (field, value, nuid))
 
-        cursor.execute("UPDATE items SET %s = '%s' WHERE id = '%s'" % (field, value, nuid))
-
-        cursor.close()
+        cur.close()
 
     res = { "status": 200, "message": "Successfully updated item." }
 
